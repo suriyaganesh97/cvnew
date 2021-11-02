02e6436f2bd53f519649309cffb266d2d4ca77b9
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-02 09:46:14 +0530

    js new commit

--- testJS/test.js
+++ testJS/test.js
@@ -1,3 +1,6 @@
-var a=8, b=7;
-c=a+b;
-alert(c);
+document.getElementById('testid').innerHTML="content";
+document.getElementsByClassName('testclass')[0].innerHTML="class content";
+document.querySelector('.pyclass').style.color="red";
+document.querySelector('li a').style.color="green";
+document.querySelectorAll('li')[0].style.backgroundColor="purple";
+// console.log(domclass)
